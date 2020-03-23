566fcaaaa65f1189c9c7e95074b20640069000b9
Karan <karanjadav@knovator.in>
2020-03-23 19:17:02 +0530

    init

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -66,5 +66,6 @@ return array(
     'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Cron\\' => array($vendorDir . '/dragonmantank/cron-expression/src/Cron'),
     'Carbon\\' => array($vendorDir . '/nesbot/carbon/src/Carbon'),
+    'Caffeinated\\Modules\\' => array($vendorDir . '/caffeinated/modules/src'),
     'App\\' => array($baseDir . '/app'),
 );
